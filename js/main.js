26b0f143ce4548f9c7a0708f230f8ed724cbef8a
Erick14911 <erickdlr1945@gmail.com>
2024-02-14 18:42:22 -0600

    003-Erick-14-02

--- js/main.js
+++ js/main.js
@@ -57,7 +57,21 @@ window.addEventListener('load', () => {
     let changes = event.target;
     document.body.classList.toggle('dark');
     changes.classList.toggle('active');
+
+    if(document.body.classList.contains('dark')){
+      localStorage.setItem('dark-mode','true');
+    }else{
+      localStorage.setItem('dark-mode','false');
+    }
     }, false);
+
+  if(localStorage.getItem('dark-mode')==='true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+  }else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+  }
   /*
   // Ocultar o Mostrar password
   let togglePassword = document.querySelector("#togglePassword");
